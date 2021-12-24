0a69273b5013928bf106dc78518cfd21027783c6
Bret Beheim <beheim@gmail.com>
2021-12-24 22:21:22 +0100

    got it down to 24 lines of code

--- day24_input_refactor.R
+++ day24_input_refactor.R
@@ -1,56 +1,28 @@
-w <- ins[1]
-x1 <- as.integer(!((z %% 26L) + 13L == w))
-z <- z + x1 * (z * 25L + w + 13L)
-
-w <- ins[2]
-x2 <- as.integer(!((z %% 26L) + 11L == w))
-z <- z + x2 * (z * 25L + w + 10L)
-
-w <- ins[3]
-x3 <- as.integer(!((z %% 26L) + 15L == w))
-z <- z + x3 * (z * 25L + w + 5L)
-
-w <- ins[4]
-x4 <- as.integer(!((z %% 26L) - 11L == w))
-z <- (z %/% 26L) + x4 * ((z %/% 26L) * 25L * x4 + w + 14L)
-
-w <- ins[5]
-x5 <- as.integer(!((z %% 26L) + 14L == w))
-z <- z + x5 * (z * 25L * x5 + w + 5L)
-
-w <- ins[6]
-x6 <- as.integer(!((z %% 26L) + 0L == w))
-z <- (z %/% 26L) + x6 * ((z %/% 26L) * 25L + w + 15L)
-
-w <- ins[7]
-x7 <- as.integer(!((z %% 26L) + 12L == w))
-z <- z + x7 * (z * 25L + w + 4L)
-
-w <- ins[8]
-x8 <- as.integer(!((z %% 26L) + 12L == w))
-z <- z + x8 * (z * 25L + w + 11L)
-
-w <- ins[9]
-x9 <- as.integer(!((z %% 26L) + 14L == w))
-z <- z + x9 * (z * 25L + w + 1L)
-
-w <- ins[10]
-x10 <- as.integer(!((z %% 26L) - 6L == w))
-z <- (z %/% 26L) + x10 * ((z %/% 26L) * 25L + w + 15L)
-
-w <- ins[11]
-x11 <- as.integer(!((z %% 26L) - 10L == w))
-z <- (z %/% 26L) + x11 * ((z %/% 26L) * 25L + w + 12L)
-
-w <- ins[12]
-x12 <- as.integer(!((z %% 26L) - 12L == w))
-z <- (z %/% 26L) + x12 * ((z %/% 26L) * 25L + w + 8L)
-
-w <- ins[13]
-x13 <- as.integer(!((z %% 26L) - 3L == w))
-z <- (z %/% 26L) + x13 * ((z %/% 26L) * 25L + w + 14L)
-
-w <- ins[14]
-x14 <- as.integer(!((z %% 26L) - 5L == w))
-z <- (z %/% 26L) + x14 * ((z %/% 26L) * 25L + w + 9L)
-
+x1 <- as.integer(!((z %% 26L) + 13L == ins[1]))
+z <- z + x1 * (z * 25L + ins[1] + 13L)
+x2 <- as.integer(!((z %% 26L) + 11L == ins[2]))
+z <- z + x2 * (z * 25L + ins[2] + 10L)
+x3 <- as.integer(!((z %% 26L) + 15L == ins[3]))
+z <- z + x3 * (z * 25L + ins[3] + 5L)
+x4 <- as.integer(!((z %% 26L) - 11L == ins[4]))
+z <- (z %/% 26L) + x4 * ((z %/% 26L) * 25L * x4 + ins[4] + 14L)
+x5 <- as.integer(!((z %% 26L) + 14L == ins[5]))
+z <- z + x5 * (z * 25L * x5 + ins[5] + 5L)
+x6 <- as.integer(!((z %% 26L) + 0L == ins[6]))
+z <- (z %/% 26L) + x6 * ((z %/% 26L) * 25L + ins[6] + 15L)
+x7 <- as.integer(!((z %% 26L) + 12L == ins[7]))
+z <- z + x7 * (z * 25L + ins[7] + 4L)
+x8 <- as.integer(!((z %% 26L) + 12L == ins[8]))
+z <- z + x8 * (z * 25L + ins[8] + 11L)
+x9 <- as.integer(!((z %% 26L) + 14L == ins[9]))
+z <- z + x9 * (z * 25L + ins[9] + 1L)
+x10 <- as.integer(!((z %% 26L) - 6L == ins[10]))
+z <- (z %/% 26L) + x10 * ((z %/% 26L) * 25L + ins[10] + 15L)
+x11 <- as.integer(!((z %% 26L) - 10L == ins[11]))
+z <- (z %/% 26L) + x11 * ((z %/% 26L) * 25L + ins[11] + 12L)
+x12 <- as.integer(!((z %% 26L) - 12L == ins[12]))
+z <- (z %/% 26L) + x12 * ((z %/% 26L) * 25L + ins[12] + 8L)
+x13 <- as.integer(!((z %% 26L) - 3L == ins[13]))
+z <- (z %/% 26L) + x13 * ((z %/% 26L) * 25L + ins[13] + 14L)
+x14 <- as.integer(!((z %% 26L) - 5L == ins[14]))
+z <- (z %/% 26L) + x14 * ((z %/% 26L) * 25L + ins[14] + 9L)
