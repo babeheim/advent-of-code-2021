0e2356dee108dbee01da649763b95334698e2918
Bret Beheim <beheim@gmail.com>
2021-12-25 00:09:36 +0100

    figued out the problem, you just have to close every gate

--- day24_input_refactor.R
+++ day24_input_refactor.R
@@ -1,23 +1,32 @@
-z <- as.numeric(((ins[1] + 13L) * 26L + ins[2] + 10L) * 26L + ins[3] + 5L)
-x4 <- as.integer(!((ins[3] + 5L) - 11L == ins[4])) # could be 1 or 0
-z <- (z %/% 26L) + x4 * ((z %/% 26L) * 25L * x4 + ins[4] + 14L)
-x5 <- as.integer(!((z %% 26L) + 14L == ins[5]))
-z <- z + x5 * (z * 25L * x5 + ins[5] + 5L)
-x6 <- as.integer(!((z %% 26L) + 0L == ins[6]))
-z <- (z %/% 26L) + x6 * ((z %/% 26L) * 25L + ins[6] + 15L)
-x7 <- as.integer(!((z %% 26L) + 12L == ins[7]))
-z <- z + x7 * (z * 25L + ins[7] + 4L)
-x8 <- as.integer(!((z %% 26L) + 12L == ins[8]))
-z <- z + x8 * (z * 25L + ins[8] + 11L)
-x9 <- as.integer(!((z %% 26L) + 14L == ins[9]))
-z <- z + x9 * (z * 25L + ins[9] + 1L)
-x10 <- as.integer(!((z %% 26L) - 6L == ins[10]))
-z <- (z %/% 26L) + x10 * ((z %/% 26L) * 25L + ins[10] + 15L)
-x11 <- as.integer(!((z %% 26L) - 10L == ins[11]))
-z <- (z %/% 26L) + x11 * ((z %/% 26L) * 25L + ins[11] + 12L)
-x12 <- as.integer(!((z %% 26L) - 12L == ins[12]))
-z <- (z %/% 26L) + x12 * ((z %/% 26L) * 25L + ins[12] + 8L)
-x13 <- as.integer(!((z %% 26L) - 3L == ins[13]))
-z <- (z %/% 26L) + x13 * ((z %/% 26L) * 25L + ins[13] + 14L)
-x14 <- as.integer(!((z %% 26L) - 5L == ins[14]))
-z <- (z %/% 26L) + x14 * ((z %/% 26L) * 25L + ins[14] + 9L)
+z0 <- as.numeric(ins[1] + 13L) * 26L + (ins[2] + 10L)
+# ins[1] sets the initial count for 26s (14:22)
+# ins[2] controls initial remainder (11:19)
+g1 <- as.integer(!(ins[3] - 6L == ins[4])) # g1=1 if ins[3] != ins[4] + 6
+stopifnot(g1 == 0)
+g2 <- as.integer(!(ins[5] + 5L == ins[6])) # g2=1 if ins[5] != ins[6] - 5
+stopifnot(g2 == 0)
+
+z6 <- z0 +
+  g1 * z0 * 25L + g1 * (ins[4] + 14L) +
+  g2 * z0 * 25L + g2 * (ins[6] + 15L) +
+  g1 * g2 * (25L * 25L * z0 + 25L * (ins[4] + 14L))
+
+z7 <- z6 * 26L + (ins[7] + 4L) # ins[7] controls remainder for z7, 5:13
+z8 <- z7 * 26L + (ins[8] + 11L) # ins[8] controls remainder for z8, 12:20
+
+g3 <- as.integer(!(ins[9] - 5L == ins[10])) # g3=1 if ins[9] != ins[10] + 5
+stopifnot(g3 == 0)
+z10 <- z8 + g3 * (z8 * 25L + (ins[10] + 15L))
+x11 <- as.integer(!((z10 %% 26L) - 10L == ins[11])) # two possibilities here; if g3 is 1, this could be 0 or 1. if g3 is 0, this must be 1
+stopifnot(x11 == 0)
+z11 <- (z10 %/% 26L) + x11 * ((z10 %/% 26L) * 25L + ins[11] + 12L) # (z10 %/% 26L) could be z8 or z7
+x12 <- as.integer(!((z11 %% 26L) - 12L == ins[12])) # how to close this?? ins[7] to 9, ins[12] to 1
+stopifnot(x12 == 0)
+z12 <- (z11 %/% 26L) + x12 * ((z11 %/% 26L) * 25L + ins[12] + 8L)
+
+x13 <- as.integer(!((z12 %% 26L) - 3L == ins[13])) # ins[13] and ins[2], would be nice if ins[13] could be 9...
+stopifnot(x13 == 0)
+z13 <- (z12 %/% 26L) + x13 * ((z12 %/% 26L) * 25L + ins[13] + 14L)
+x14 <- as.integer(!((z13 %% 26L) - 5L == ins[14]))
+stopifnot(x14 == 0)
+z <- (z13 %/% 26L) + x14 * ((z13 %/% 26L) * 25L + ins[14] + 9L)
--- day24_scratch.md
+++ day24_scratch.md
@@ -229,6 +229,9 @@ x <- run_monad("day24_input.txt", 11111111111111)$z[252]
 run_monadR("day24_input.R", 11111111111111)$z == x
 run_monadR("day24_input.R", 11111111111111)$z == x
 
+
+run_monadR("day24_input_refactor.R", 11111111111111)
+
 # the biggest integer the machine can represent is 2147483647, only 10 digits!
 for (i in 1:1000) {
   seed <- as.numeric(paste(sample(1:9, 14, replace = TRUE), collapse = ""))
@@ -242,3 +245,52 @@ for (i in 1:1000) {
 ```
 
 With the code refactored, we can finally see the structure of the MONAD program. We start with z = 0. Each digit of input serves as a logical gate. The first three gates, x1 to x3, MUST be 1. The fourth could be 1 or 0
+
+turn all gates off!
+
+```r
+# 98124919943911 works
+# 99124919943921 works too! i can get this later...i have the problem solved!
+input <- 99124919943921
+ins <- rev(as.integer(strsplit(as.character(input), "")[[1]]))
+
+z0 <- as.numeric(ins[1] + 13L) * 26L + (ins[2] + 10L)
+# ins[1] sets the initial count for 26s (14:22)
+# ins[2] controls initial remainder (11:19)
+g1 <- as.integer(!(ins[3] - 6L == ins[4])) # g1=1 if ins[3] != ins[4] + 6
+stopifnot(g1 == 0)
+g2 <- as.integer(!(ins[5] + 5L == ins[6])) # g2=1 if ins[5] != ins[6] - 5
+stopifnot(g2 == 0)
+
+z6 <- z0 +
+  g1 * z0 * 25L + g1 * (ins[4] + 14L) +
+  g2 * z0 * 25L + g2 * (ins[6] + 15L) +
+  g1 * g2 * (25L * 25L * z0 + 25L * (ins[4] + 14L))
+
+z7 <- z6 * 26L + (ins[7] + 4L) # ins[7] controls remainder for z7, 5:13
+z8 <- z7 * 26L + (ins[8] + 11L) # ins[8] controls remainder for z8, 12:20
+
+g3 <- as.integer(!(ins[9] - 5L == ins[10])) # g3=1 if ins[9] != ins[10] + 5
+stopifnot(g3 == 0)
+z10 <- z8 + g3 * (z8 * 25L + (ins[10] + 15L))
+x11 <- as.integer(!((z10 %% 26L) - 10L == ins[11])) # two possibilities here; if g3 is 1, this could be 0 or 1. if g3 is 0, this must be 1
+stopifnot(x11 == 0)
+z11 <- (z10 %/% 26L) + x11 * ((z10 %/% 26L) * 25L + ins[11] + 12L) # (z10 %/% 26L) could be z8 or z7
+x12 <- as.integer(!((z11 %% 26L) - 12L == ins[12])) # how to close this?? ins[7] to 9, ins[12] to 1
+stopifnot(x12 == 0)
+z12 <- (z11 %/% 26L) + x12 * ((z11 %/% 26L) * 25L + ins[12] + 8L)
+
+x13 <- as.integer(!((z12 %% 26L) - 3L == ins[13])) # ins[13] and ins[2], would be nice if ins[13] could be 9...
+stopifnot(x13 == 0)
+z13 <- (z12 %/% 26L) + x13 * ((z12 %/% 26L) * 25L + ins[13] + 14L)
+x14 <- as.integer(!((z13 %% 26L) - 5L == ins[14]))
+stopifnot(x14 == 0)
+(z13 %/% 26L) + x14 * ((z13 %/% 26L) * 25L + ins[14] + 9L)
+
+# valid only if z = 0, so z13 is LESS than 26
+# ah hah, so you have to CLOSE EVERY GATE and keep them closed...
+
+# strategy: turn all gates off!
+
+```
+
